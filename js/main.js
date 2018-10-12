6e190d8af8e56455a8242b1c5d689f0d8b6cb0a7
Vladimir Kamuz <v.kamuz@gmail.com>
2018-10-12 17:32:03 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function() {
+
+    // Smooth scroll navigation init
+    scrollSpy(document.querySelector('.navbar'));
+
+    // Smooth scroll anchor init
+    smoothScroll.init();
+
+});
