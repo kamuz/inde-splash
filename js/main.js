f0680c4fd5c20ccdb0ed4432c603369cf5a36151
Vladimir Kamuz <v.kamuz@gmail.com>
2018-10-17 02:11:11 +0300

    Implement mobile menu & other mobile style (not complete)

--- js/main.js
+++ js/main.js
@@ -7,3 +7,24 @@ window.addEventListener('load', function() {
     smoothScroll.init();
 
 });
+
+/**
+ * Top Mobile Menu
+ */
+
+var toggleButton = document.getElementById("menu-links");
+var menuLink = document.querySelectorAll('.navbar #menu-links ul li a');
+
+function toggleMenu() {
+    if (toggleButton.style.display === "block") {
+        toggleButton.style.display = "none";
+    } else {
+        toggleButton.style.display = "block";
+    }
+}
+
+for (var i = 0; i < menuLink.length; i++) {
+    menuLink[i].addEventListener('click', function(event) {
+        toggleButton.style.display = "none";
+    });
+}
